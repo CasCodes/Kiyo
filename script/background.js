48bd81439a5a47fcd63133c5c56fa86da982cefa
CAS <cas@pop-os.localdomain>
2022-03-03 14:54:11 +0100

    pop up can talk to content

--- script/background.js
+++ script/background.js
@@ -1,3 +1,5 @@
+// background.js triggers when the widget is clicked
+// get's canceled by the popup
 
 console.log("I'm running back here!")
 
--- script/content.js
+++ script/content.js
@@ -1,8 +1,9 @@
+// content.js triggers when the page gets loaded
 
 console.log("Kiyo online!")
 
 chrome.runtime.onMessage.addListener(gotMessage)
 
 function gotMessage(message, sender, sendResponse) {
-    console.log(message.txt);
+    console.log(message);
 }
