24c7dfb439fc2e21c70a826b9d789af61b5b005b
CAS <masterbytwo@gmail.com>
2022-03-06 17:10:03 +0100

    removed after request in app.py

--- API/src/app.py
+++ API/src/app.py
@@ -26,13 +26,13 @@ api.add_resource(Summary, "/summary/<string:text>")
 if __name__ == "__main__":
     app.run(debug=True)
 
-@app.after_request
-def after_request(response):
-    white_origin= ['http://localhost']
-    if request.headers['Origin'] in white_origin:
-        response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] 
-        response.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
-    print("after:")
-    print(response)
-    return response
+# @app.after_request
+# def after_request(response):
+#     white_origin= ['http://localhost']
+#     if request.headers['Origin'] in white_origin:
+#         response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] 
+#         response.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
+#         response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+#     print("after:")
+#     print(response)
+#     return response
