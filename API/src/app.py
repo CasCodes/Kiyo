8aae6a17231dfc827af791f7ef9d3c2bdb62a9f1
CAS <masterbytwo@gmail.com>
2022-03-08 18:18:48 +0100

    changed model

--- API/src/app.py
+++ API/src/app.py
@@ -18,6 +18,7 @@ class Summary(Resource):
         
         # access the ML model
         summary = compute_summarize(text, model)
+        
         print(summary)
         return jsonify(summary)
 
