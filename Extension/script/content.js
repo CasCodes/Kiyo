7137d98e98bcf487516abb2d0be6d75b2cd1b7ec
CAS <masterbytwo@gmail.com>
2022-03-11 11:29:00 +0100

    cute little pixel sprite uwu

--- Extension/script/content.js
+++ Extension/script/content.js
@@ -34,3 +34,10 @@ function gotMessage(message, sender, sendResponse) {
         readSelection();
     }
 }
+
+// "GET /summary/The%20climax%20of%20this%20course%20is%20its%20final%20project.
+// %20The%20final%20project%20is%20your%20opportunity%20to%20take%20your%20newfound%
+// 20savvy%20with%20programming%20out%20for%20a%20spin%20and%20develop%20your%20very%20own%
+// 20piece%20of%20software.%20So%20long%20as%20your%20project%20draws%20upon%20this%20course’s%
+// 20lessons,%20the%20nature%20of%20your%20project%20is%20entirely%20up%20to%20you.%20You%20may%
+// 20implement%20your%20project%20in%20any%20language(s). HTTP/1.1" 200 -
